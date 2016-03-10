e58057fa212b6b6641632192df043db8d90bcb0c
sklthegooman <10909568@qq.com>
2016-03-10 20:14:34 +0800

    add searchBar

--- dist/example/example.js
+++ dist/example/example.js
@@ -97,5 +97,37 @@ $(function () {
 
     // tab
     $('.weui_tab').tab();
+
+    // searchBar
+    $('.search_bar_wrap').searchBar({
+        //替换原模板的“取消”
+        cancelText:"取消",
+        //替换原模板的“搜索”
+        searchText:'搜索',
+        //搜索栏获得焦点时
+        onfocus: function (value) {
+            console.log('focus!The value is '+value);
+        },
+        //搜索栏失去焦点时
+        onblur:function(value) {
+            console.log('blur!The value is '+value);
+        },
+        //搜索栏在输入时
+        oninput: function(value) {
+            console.log('Input!The value is '+ value);
+        },
+        //搜索栏提交时，如果没有submit方法，则沿用浏览器默认的提交方式
+        onsubmit:function(value){
+            console.log('Submit!The value is '+ value);
+        },
+        //点击取消按钮
+        oncancel:function(){
+            console.log('click cancel');
+        },
+        //点击清空按钮
+        onclear:function(){
+            console.log('click clear');
+        }
+    });
 });
 
